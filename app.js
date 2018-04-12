b72678178167bcadd4d2937053cc6e36995965e3
justincrywolf <justincrywolf@gmail.com>
2018-04-12 13:21:53 -0400

    here's some pseudo code

---
+++ app.js
@@ -0,0 +1,17 @@
+// Dependencies
+var express = require( "express" );
+
+var app = express();
+var PORT = process.env || 3000;
+
+// Data
+
+// Guests
+
+    // Reservations
+
+    // Waitlist
+
+// Routes
+
+// Listener
